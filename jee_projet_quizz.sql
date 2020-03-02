f1faf8e8f1a39814aab19049a0bb4ed32e0c49c0
MORELVincent <43674711+MORELVincent@users.noreply.github.com>
2020-03-02 18:49:40 +0100

    Ajout de quelques questions pour les quizzs

--- jee_projet_quizz.sql
+++ jee_projet_quizz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 01 mars 2020 à 22:28
+-- Généré le :  lun. 02 mars 2020 à 17:42
 -- Version du serveur :  5.7.21
 -- Version de PHP :  5.6.35
 
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `question` (
   `intitule` varchar(255) NOT NULL,
   `quizz_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `question`
@@ -70,7 +70,22 @@ INSERT INTO `question` (`id`, `intitule`, `quizz_id`) VALUES
 (4, 'Java est un langage développé par ... ?', 2),
 (5, 'Pourquoi les chats miaulent-ils ?', 3),
 (6, 'Il faut promener son chien :', 3),
-(7, 'Le lapin a une vision de :', 3);
+(7, 'Le lapin a une vision de :', 3),
+(8, 'Lequel des éléments suivants n’est pas un concept POO en Java ?', 2),
+(9, 'Quand la surcharge de méthode est-elle déterminée ?', 2),
+(10, 'Quand la surcharge ne se produit pas ?', 2),
+(11, 'Quel concept de Java est un moyen de convertir des objets du monde réel en termes de classe ?', 2),
+(12, 'Comment s’appelle-t-on dans le cas où l’objet d’une classe mère est détruit donc l’objet d’une classe fille sera détruit également ?', 2),
+(13, ' L’interprétation des programmes Java est effectuée par :', 2),
+(14, 'Le marcassin est un', 3),
+(15, 'Comment se nomme la femelle du lièvre ?', 3),
+(16, 'Lequel de ces oiseaux peut voler à reculons ?', 3),
+(17, 'Quel poisson possède environ 700 dents ?', 3),
+(18, 'Combien de tentacules se trouvent sur la tête de la limace ?', 3),
+(19, 'Quel mammifère est surnommé la licorne de mer ?', 3),
+(20, 'Quelle annotation lie un paramètre de requête JPA à un argument de méthode ?', 1),
+(21, 'Parmi les 4 réponses, qu\'est-ce qui agit comme un Front Controller dans Sptring Web App ?', 1),
+(22, 'Qu\'est-ce que l’injection de dépendance ?', 1);
 
 -- --------------------------------------------------------
 
@@ -107,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `reponsesquestions` (
   `est_correct` tinyint(1) NOT NULL,
   `question_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=89 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `reponsesquestions`
@@ -141,7 +156,67 @@ INSERT INTO `reponsesquestions` (`id`, `reponse`, `est_correct`, `question_id`) 
 (25, '90°\r\n', 0, 7),
 (26, '180°\r\n', 0, 7),
 (27, '340°\r\n', 1, 7),
-(28, '360°\r\n', 0, 7);
+(28, '360°\r\n', 0, 7),
+(29, 'Héritage', 0, 8),
+(30, 'Encapsulation', 0, 8),
+(31, 'Polymorphisme', 0, 8),
+(32, 'Compilation', 1, 8),
+(33, 'Au moment de l\'exécution', 0, 9),
+(34, 'Au moment de la compilation', 1, 9),
+(35, 'Au moment du codage', 0, 9),
+(36, 'Au moment de l\'éxécution', 0, 9),
+(37, 'Quand il y a plusieurs méthodes avec le même nom mais avec une signature de méthode différente et un nombre ou un type de paramètres différent', 0, 10),
+(38, 'Quand il y a plusieurs méthodes avec le même nom, le même nombre de paramètres et le type mais une signature différente', 1, 10),
+(39, 'Quand il y a plusieurs méthodes avec le même nom, la même signature, le même nombre de paramètres mais un type différent', 0, 10),
+(40, 'Quand il y a plusieurs méthodes avec le même nom, la même signature mais avec différente signature', 0, 10),
+(41, 'Polymorphisme', 0, 11),
+(42, 'Encapsulation', 0, 11),
+(43, 'Abstraction', 1, 11),
+(44, 'Héritage', 0, 11),
+(45, 'Agrégation', 0, 12),
+(46, 'Composition', 1, 12),
+(47, 'Encapsulation', 0, 12),
+(48, 'Association', 0, 12),
+(49, 'API', 0, 13),
+(50, 'JDK', 0, 13),
+(51, 'JVM', 1, 13),
+(52, 'AWT', 0, 13),
+(53, 'Oiseau', 0, 14),
+(54, 'Crustacé', 0, 14),
+(55, 'Reptile', 0, 14),
+(56, 'Mammifère', 1, 14),
+(57, 'La lévrière', 0, 15),
+(58, 'La hase', 1, 15),
+(59, 'La lapine', 0, 15),
+(60, 'La lièvre', 0, 15),
+(61, 'La mésange', 0, 16),
+(62, 'Le rouge gorge', 0, 16),
+(63, 'Le colibri', 1, 16),
+(64, 'Le moineau', 0, 16),
+(65, 'Le brochet', 1, 17),
+(66, 'La truite', 0, 17),
+(67, 'La carpe', 0, 17),
+(68, 'Le goujon', 0, 17),
+(69, 'Onze', 0, 18),
+(70, 'Deux', 0, 18),
+(71, 'Quatre', 1, 18),
+(72, 'Huit', 0, 18),
+(73, 'Le requin blanc', 0, 19),
+(74, 'Le narval', 1, 19),
+(75, 'L\'espadon', 0, 19),
+(76, 'La licorne', 0, 19),
+(77, '@Param', 1, 20),
+(78, '@QueryParam', 0, 20),
+(79, '@RequestParam', 0, 20),
+(80, '@QueryParameter', 0, 20),
+(81, '@RestController', 0, 21),
+(82, 'DispatcherServlet', 1, 21),
+(83, 'Models', 0, 21),
+(84, 'View', 0, 21),
+(85, 'C\'est un design pattern qui implémente le pattern Inversion de contrôle(inversion of control, IoC) pour des applications logicielles', 1, 22),
+(86, 'C\'est l’un des modules de Spring', 0, 22),
+(87, ' C\'est une technique pour obtenir des dépendances de n\'importe quel projet', 0, 22),
+(88, 'Il est utilisé pour promouvoir un couplage étroit dans le code', 0, 22);
 
 -- --------------------------------------------------------
 
@@ -154,15 +229,7 @@ CREATE TABLE IF NOT EXISTS `reponsesutilisateurs` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `reponse_question_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=166 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `reponsesutilisateurs`
---
-
-INSERT INTO `reponsesutilisateurs` (`id`, `reponse_question_id`) VALUES
-(165, 14),
-(164, 10);
+) ENGINE=MyISAM AUTO_INCREMENT=187 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
